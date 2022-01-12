e8f0c729c94de09b597384d5c6d72bf993ca07ec
Chloe Peng <chloepeng331@icloud.com>
2022-01-11 23:00:43 -0500

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"];
+const events = "birthday";
+
+function writeCards (names, events) {
+    for (let i = 0; i < names.length; i++) {
+        console.log (`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+    }
+    return names;
+}
+
+writeCards(names, events);
+
+
+
+let countDown = 10;
+
+while (countDown >= 0){
+    console.log (countDown--);
+}
+
+
+/*let countup = 0;
+while (countup < 10) {
+  console.log(countup++);
+}
+
+*/
